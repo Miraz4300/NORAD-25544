c7240dd0c58b4a3b7d50ddf386c78bb7ba1ebd6d
Miraz4300 <miraz4300@gmail.com>
2022-10-23 12:03:37 +0600

    new-relic config

---
+++ metrics.sh
@@ -0,0 +1,1 @@
+curl -Ls https://download.newrelic.com/install/newrelic-cli/scripts/install.sh | bash && sudo NEW_RELIC_API_KEY=KEY NEW_RELIC_ACCOUNT_ID=ID /usr/local/bin/newrelic install -n logs-integration
